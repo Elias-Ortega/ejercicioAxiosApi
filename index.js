13101cf5f87cb73a9f2eb4cd0a7a61466c575060
Elias Ortega <eliasortega2011@gmail.com>
2023-06-26 19:12:30 -0400

    Busqueda pokemon aleatorio

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const fs = require('fs/promises');
+const axios = require('axios');
+
+const numeroAzar = (numeroMaximo) => Math.floor(Math.random() * numeroMaximo - 1);
+
+const obtenerPokemon  = async () => {
+    const datos = await fs.readFile('datos.txt');
+    const arrayDatos = JSON.parse(datos);
+
+    const numeroArray = Math.abs(numeroAzar(arrayDatos.length));
+
+    const valorBusqueda = arrayDatos[numeroArray];
+
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${valorBusqueda}`);
+
+    console.log(data);
+}
+
+obtenerPokemon();
